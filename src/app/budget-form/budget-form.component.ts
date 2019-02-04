dd0c72b4ab60b6545504dc1270476e1e1e92be89
staudinger <staudinger@mail.usf.edu>
2019-02-04 10:21:23 -0500

    finished form

--- src/app/budget-form/budget-form.component.ts
+++ src/app/budget-form/budget-form.component.ts
@@ -3,7 +3,8 @@ import { BudgetService } from '../budget.service';
 import { Budget } from '../budget';
 import { Router } from '@angular/router';
 
-import { FormControl, Validators} from '@angular/forms';
+import {  FormGroup, FormControl, Validators} from '@angular/forms';
+import { MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-budget-form',
@@ -12,110 +13,123 @@ import { FormControl, Validators} from '@angular/forms';
 })
 export class BudgetFormComponent implements OnInit {
 
+  form: FormGroup;
+
   budget = {} as Budget;
   budgets: Budget[] = [];
 
-  clickSubmit(): void
-  {
-    this.budget.totalExpense = this.budget.saving + this.budget.debts + this.budget.housing + this.budget.insurance + this.budget.medicalAndHealth + this.budget.personal + this.budget.recreation + this.budget.transportation + this.budget.utilities + this.budget.charitableGifts + this.budget.clothing + this.budget.food;
-    this.budgetService.addBudget(this.budget).subscribe(
-      budget=>
-      {
-        console.log(this.budget);
-      }
-    );
-    this.router.navigate(['/budgets']);
-  }
 
+ 
 
-  income = new FormControl('', [Validators.required]);
 
   getErrorMessageIncome() {
-    return this.income.hasError('required') ? 'You must enter an income': '';
+    return this.form.get('income').errors['required'] ? 'You must enter an income': '';
   }
 
-  charitableGifts = new FormControl('', [Validators.required]);
 
   getErrorMessageCharity() {
-    return this.charitableGifts.hasError('required') ? 'You must enter an amount': '';
+    return this.form.get('charitableGifts').errors['required'] ? 'You must enter an amount': '';
   }
 
-  savings = new FormControl('', [Validators.required]);
 
   getErrorMessageSaving() {
-    return this.savings.hasError('required') ? 'You must enter an amount': '';
+    return this.form.get('savings').errors['required'] ? 'You must enter an amount': '';
   }
 
-  housing = new FormControl('', [Validators.required]);
 
   getErrorMessageHousing() {
-    return this.housing.hasError('required') ? 'You must enter an amount': '';
+    return this.form.get('housing').errors['required'] ? 'You must enter an amount': '';
   }
 
-  utilities = new FormControl('', [Validators.required]);
 
   getErrorMessageUtilities() {
-    return this.utilities.hasError('required') ? 'You must enter an amount': '';
+    return this.form.get('utilities').errors['required'] ? 'You must enter an amount': '';
   }
 
-  food = new FormControl('', [Validators.required]);
 
   getErrorMessageFood() {
-    return this.food.hasError('required') ? 'You must enter an amount': '';
+    return this.form.get('food').errors['required'] ? 'You must enter an amount': '';
   }
 
-  clothing = new FormControl('', [Validators.required]);
 
   getErrorMessageClothing() {
-    return this.clothing.hasError('required') ? 'You must enter an amount': '';
+    return this.form.get('clothing').errors['required'] ? 'You must enter an amount': '';
   }
 
-  transportation = new FormControl('', [Validators.required]);
 
   getErrorMessageTransportation() {
-    return this.transportation.hasError('required') ? 'You must enter an amount': '';
+    return this.form.get('transportation').errors['required'] ? 'You must enter an amount': '';
   }
 
-  medicalAndHealth = new FormControl('', [Validators.required]);
 
   getErrorMessageMedical() {
-    return this.medicalAndHealth.hasError('required') ? 'You must enter an amount': '';
+    return this.form.get('medicalAndHealth').errors['required'] ? 'You must enter an amount': '';
   }
 
-  insurance = new FormControl('', [Validators.required]);
 
   getErrorMessageInsurance() {
-    return this.insurance.hasError('required') ? 'You must enter an amount': '';
+    return this.form.get('insurance').errors['required'] ? 'You must enter an amount': '';
   }
 
-  personal = new FormControl('', [Validators.required]);
 
   getErrorMessagePersonal() {
-    return this.personal.hasError('required') ? 'You must enter an amount': '';
+    return this.form.get('personal').errors['required'] ? 'You must enter an amount': '';
   }
 
-  recreation = new FormControl('', [Validators.required]);
 
   getErrorMessageRecreation() {
-    return this.recreation.hasError('required') ? 'You must enter an amount': '';
+    return this.form.get('recreation').errors['required'] ? 'You must enter an amount': '';
   }
 
-  debts = new FormControl('', [Validators.required]);
 
   getErrorMessageDebts() {
-    return this.debts.hasError('required') ? 'You must enter an amount': '';
+    return this.form.get('debts').errors['required'] ? 'You must enter an amount': '';
   }
 
-  month = new FormControl('', [Validators.required]);
 
   getErrorMessageMonth() {
-    return this.month.hasError('required') ? 'You must enter a month': '';
+    return this.form.get('month').errors['required'] ? 'You must enter a month': '';
   }
 
-  constructor(private budgetService: BudgetService, private router:Router) { }
+  constructor(private budgetService: BudgetService, private router:Router, private dialogRef: MatDialogRef<BudgetFormComponent>) { }
 
   ngOnInit()
   {
+    this.form = new FormGroup(
+      {
+        'income': new FormControl('', [Validators.required]),
+        'charitableGifts': new FormControl('', [Validators.required]),
+        'savings' : new FormControl('', [Validators.required]),
+        'housing' : new FormControl('', [Validators.required]),
+        'utilities' : new FormControl('', [Validators.required]),
+        'food' : new FormControl('', [Validators.required]),
+        'clothing' : new FormControl('', [Validators.required]),
+        'transportation' : new FormControl('', [Validators.required]),
+        'medicalAndHealth' : new FormControl('', [Validators.required]),
+        'insurance' : new FormControl('', [Validators.required]),
+        'personal' : new FormControl('', [Validators.required]),
+        'recreation' : new FormControl('', [Validators.required]),
+        'debts' : new FormControl('', [Validators.required]),
+        'month' : new FormControl('', [Validators.required])
+      
+      }
+      
+    )
+    
   }
 
+  clickSubmit(): void
+  {
+    if(this.form.valid){
+      this.budget.totalExpense = this.budget.saving + this.budget.debts + this.budget.housing + this.budget.insurance + this.budget.medicalAndHealth + this.budget.personal + this.budget.recreation + this.budget.transportation + this.budget.utilities + this.budget.charitableGifts + this.budget.clothing + this.budget.food;
+      this.budgetService.addBudget(this.budget).subscribe(
+        budget=>
+        {
+          console.log(this.budget);
+        }
+      );
+      this.dialogRef.close(this.budget);
+      this.router.navigate(['/budgets']);
+    }
+  }
 }
